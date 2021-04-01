2c074963950424a8a27fa72959a0a401257c18b9
SamDanielEngineering <SamDanielEngineering@gmail.com>
2021-04-01 14:54:36 -0400

    Fixed ReimbursementDaoTestCases JUNIT class

--- src/test/java/com/projects/onePointFive/dao/ReimbursementDaoTestCases.java
+++ src/test/java/com/projects/onePointFive/dao/ReimbursementDaoTestCases.java
@@ -9,41 +9,39 @@ import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ReimbursementDaoTestCases {
-    public class UserDaoTestCases {
-        @Test
-        @Order(1)
-        public void getReimbursementList(){
+    @Test
+    @Order(1)
+    public void getReimbursementList(){
 
-        }
+    }
 
-        @Test
-        @Order(2)
-        public void getReimbursementById(){
+    @Test
+    @Order(2)
+    public void getReimbursementById(){
 
-        }
+    }
 
-        @Test
-        @Order(3)
-        public void getListOfUsersReimbursements(){
+    @Test
+    @Order(3)
+    public void getListOfUsersReimbursements(){
 
-        }
+    }
 
-        @Test
-        @Order(4)
-        public void insertReimbursement(){
+    @Test
+    @Order(4)
+    public void insertReimbursement(){
 
-        }
+    }
 
-        @Test
-        @Order(5)
-        public void updateReimbursementList(){
+    @Test
+    @Order(5)
+    public void updateReimbursementList(){
 
-        }
+    }
 
-        @Test
-        @Order(6)
-        public void deleteReimbursement(){
+    @Test
+    @Order(6)
+    public void deleteReimbursement(){
 
-        }
     }
 }
